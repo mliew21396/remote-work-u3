82a7feb3e5e26f4f23e1f4e77fa7eaf20eab620e
mechamanny <ekaunitz@gmail.com>
2014-07-08 13:52:24 -0700

    rectangle_spec from should to expect

--- week-8-and-9/3-Ruby/rectangle-class/rectangle_spec.rb
+++ week-8-and-9/3-Ruby/rectangle-class/rectangle_spec.rb
@@ -6,57 +6,57 @@ describe Rectangle do
 
   describe '#area' do
     it "is defined" do
-      Rectangle.method_defined?(:area).should be_true
+      expect(Rectangle.method_defined?(:area)).to be true
     end
 
     it "returns the correct area of a rectangle" do
-      rectangle.area.should eq 200
+      expect(rectangle.area).to eq 200
     end
 
     it "returns the correct area of a square" do
-      square.area.should eq 400
+      expect(square.area).to eq 400
     end
   end
 
   describe '#perimeter' do
     it "is defined" do
-      Rectangle.method_defined?(:perimeter).should be_true
+      expect(Rectangle.method_defined?(:perimeter)).to be true
     end
 
     it "returns the correct perimeter of a rectangle" do
-      rectangle.perimeter.should eq 60
+      expect(rectangle.perimeter).to eq 60
     end
 
     it "returns the correct perimeter of a square" do
-      square.perimeter.should eq 80
+      expect(square.perimeter).to eq 80
     end
   end
 
   describe '#diagonal' do
     it "is defined" do
-      Rectangle.method_defined?(:diagonal).should be_true
+      expect(Rectangle.method_defined?(:diagonal)).to be true
     end
 
     it "returns the correct diagonal of a rectangle" do
-      rectangle.diagonal.should eq 22.360679774997898
+      expect(rectangle.diagonal).to eq 22.360679774997898
     end
 
     it "returns the correct diagonal of a square" do
-      square.diagonal.should eq 28.284271247461902
+      expect(square.diagonal).to eq 28.284271247461902
     end
   end
 
   describe '#square?' do
     it "is defined" do
-      Rectangle.method_defined?(:square?).should be_true
+      expect(Rectangle.method_defined?(:square?)).to be true
     end
 
     it "returns false for a rectangle" do
-      rectangle.square?.should be_false
+      expect(rectangle.square?).to be_false
     end
 
     it "returns true for a square" do
-      square.square?.should be_true
+      expect(square.square?).to be true
     end
   end
 end
