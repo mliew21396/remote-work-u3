9b38bd9b9d99a307cc3270ef898db037d3eb1b35
Maria Piper <mbtomori@gmail.com>
2014-09-12 11:08:49 -0700

    added week 3 JS material

---
+++ week-3/1-code-combat/my_solution.js
@@ -0,0 +1,23 @@
+// I worked on this challenge [by myself, with:]
+
+// Write pseudocode as a comment for each mission.
+// Note: to make commenting easier, you can highlight the section you want to comment and hold
+// command + / This will comment the line.
+
+
+// 1. Rescue Mission
+
+
+
+
+// 2. Grab the Mushroom
+
+
+
+
+// 3. Drink Me
+
+
+
+// Reflection:
+// Write your reflection here.
