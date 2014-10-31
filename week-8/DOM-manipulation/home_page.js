ab088d2b15f51ad8c7bf936a09f4f1f513a2367c
mechamanny <ekaunitz@gmail.com>
2014-10-31 11:57:23 -0700

    removed JS gradebook changes from this PR

---
+++ week-8/DOM-manipulation/home_page.js
@@ -0,0 +1,37 @@
+// Unit 3 Week 8 DOM Manipulation Challenge
+// Add you JavaScript calls to this page:
+
+// Release 0:
+
+
+
+
+// Release 1:
+
+
+
+
+// Release 2:
+
+
+
+
+// Release 3:
+
+
+
+
+// Release 4:
+
+
+
+
+// Release 5:
+
+
+
+
+// Reflection:
+
+// Add a reflection here to talk about what you learned about DOM Manipulation.
+// Try to limit yourself to 10-15 minutes of reflection time.
